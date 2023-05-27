ae39ed828f16df38b3404e71263bd923016aa014
snegur04ka <barte977@mail.ru>
2023-05-27 13:18:09 +0600

    adaptive

--- src/views/notarius/Applications.vue
+++ src/views/notarius/Applications.vue
@@ -45,7 +45,130 @@
         <input type="text" class="application-bottom__input" />
       </label>
     </div>
-    <div class="application-table">
+    <div class="adaptive__sort">
+      <div class="application-mid__sort">
+        <span class="application-mid__text">Сортировка за:</span>
+        <button class="application-mid__select">
+          2022 год
+          <ArrowDownSmallIcon />
+        </button>
+      </div>
+    </div>
+    <div class="application__adaptive">
+      <div class="application__left">
+        <label class="tooltip">
+          <svg
+            width="22"
+            height="22"
+            viewBox="0 0 22 22"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M11 21C16.5228 21 21 16.5228 21 11C21 5.47715 16.5228 1 11 1C5.47715 1 1 5.47715 1 11C1 16.5228 5.47715 21 11 21Z"
+              stroke="#1BAA75"
+              stroke-width="2"
+            />
+            <path
+              d="M10.9998 16.9996C11.6625 16.9996 12.1998 16.4624 12.1998 15.7996C12.1998 15.1369 11.6625 14.5996 10.9998 14.5996C10.3371 14.5996 9.7998 15.1369 9.7998 15.7996C9.7998 16.4624 10.3371 16.9996 10.9998 16.9996Z"
+              fill="#1BAA75"
+            />
+            <path
+              d="M8.75 7.2497C8.75005 6.85567 8.85358 6.46856 9.05023 6.1271C9.24688 5.78565 9.52975 5.50183 9.87054 5.30403C10.2113 5.10623 10.5981 5.0014 10.9921 5.00001C11.3861 4.99863 11.7736 5.10075 12.1158 5.29616C12.458 5.49156 12.7428 5.77339 12.9418 6.11346C13.1409 6.45353 13.2471 6.8399 13.2499 7.23392C13.2528 7.62794 13.152 8.0158 12.9579 8.35867C12.7637 8.70155 12.4829 8.98743 12.1436 9.1877C11.5736 9.5249 11 10.0373 11 10.6997V12.1997"
+              stroke="#1BAA75"
+              stroke-width="2"
+              stroke-linecap="round"
+            /></svg
+          ><input type="checkbox" /><span
+            >Таблицу можно скролить по<br />
+            горизонтали либо Вы можете<br />
+            использовать кнопки для<br />
+            перемещения по таблице<svg
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M6.64645 6.35355L17.9602 17.6673"
+                stroke="#1BAA75"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+              <path
+                d="M6.35355 17.3536L17.6673 6.03985"
+                stroke="#1BAA75"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              /></svg
+          ></span>
+        </label>
+      </div>
+      <div class="application__right">
+        <svg
+          @click="handleScrollLeft"
+          width="40"
+          height="40"
+          viewBox="0 0 40 40"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <rect x="0.5" y="0.5" width="39" height="39" fill="white" />
+          <g clip-path="url(#clip0_2412_22742)">
+            <path
+              d="M22 16L18 20L22 24"
+              stroke="#1BAA75"
+              stroke-width="2"
+              stroke-linejoin="round"
+            />
+          </g>
+          <rect x="0.5" y="0.5" width="39" height="39" stroke="#EFEFEF" />
+          <defs>
+            <clipPath id="clip0_2412_22742">
+              <rect
+                width="16"
+                height="16"
+                fill="white"
+                transform="translate(28 12) rotate(90)"
+              />
+            </clipPath>
+          </defs>
+        </svg>
+        <svg
+          @click="handleScrollRight"
+          width="40"
+          height="40"
+          viewBox="0 0 40 40"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <rect x="0.5" y="0.5" width="39" height="39" fill="white" />
+          <g clip-path="url(#clip0_2412_22746)">
+            <path
+              d="M18 24L22 20L18 16"
+              stroke="#1BAA75"
+              stroke-width="2"
+              stroke-linejoin="round"
+            />
+          </g>
+          <rect x="0.5" y="0.5" width="39" height="39" stroke="#EFEFEF" />
+          <defs>
+            <clipPath id="clip0_2412_22746">
+              <rect
+                width="16"
+                height="16"
+                fill="white"
+                transform="matrix(0 -1 1 0 12 28)"
+              />
+            </clipPath>
+          </defs>
+        </svg>
+      </div>
+    </div>
+    <div ref="container" class="application-table">
       <table>
         <thead>
           <tr>
@@ -131,7 +254,21 @@ const isfilter = ref(true);
 
 const files = new Array(39);
 const itemsPerPage = 6;
+  const container = ref(null);
 
+  const handleScrollRight = () => {
+    container.value.scrollBy({
+      left: 200,
+      behavior: "smooth",
+    });
+  };
+
+  const handleScrollLeft = () => {
+    container.value.scrollBy({
+      left: -200,
+      behavior: "smooth",
+    });
+  };
 const paginatedItems = ref(files);
 
 const paginate = (data) => {
@@ -168,16 +305,49 @@ const thirdList = ref([
 
 <style lang="scss" scoped>
 @import "@/assets/scss/variables.scss";
-
+.adaptive {
+  &__sort {
+    .application-mid__sort {
+      display: none;
+      @media screen and (max-width: 768px) {
+        display: block;
+      }
+      .application-mid__select {
+        width: 100%;
+        margin-top: 10px;
+      }
+    }
+  }
+}
 .application {
   flex: 1;
-
+  &__adaptive {
+    display: none;
+    justify-content: space-between;
+    margin-top: 30px;
+    @media screen and (max-width: 1138px) {
+      display: flex;
+    }
+    @media screen and (max-width: 768px) {
+        margin-top: 30px;
+      }
+  }
+  &__left svg {
+    cursor: pointer;
+  }
+  &__right svg {
+    margin-left: 10px;
+    cursor: pointer;
+  }
   &-head {
     display: flex;
     align-items: center;
     gap: 30px;
     margin: 40px 0;
     max-width: 100%;
+    @media screen and (max-width: 768px) {
+        margin: 30px 0
+      }
   }
 
   &-search {
@@ -220,6 +390,9 @@ const thirdList = ref([
     gap: 10px;
     height: 50px;
     width: 300px;
+    @media screen and (max-width: 768px) {
+      display: none;
+    }
   }
 
   &-mid {
@@ -232,6 +405,9 @@ const thirdList = ref([
       display: flex;
       align-items: center;
       gap: 10px;
+      @media screen and (max-width: 768px) {
+      display: none;
+    }
     }
 
     &__btn {
@@ -245,6 +421,10 @@ const thirdList = ref([
       gap: 10px;
       padding: 10px 30px;
       width: 235px;
+      @media screen and (max-width: 768px) {
+      width: 100%;
+      justify-content: center;
+    }
     }
 
     &__text {
@@ -272,7 +452,10 @@ const thirdList = ref([
     align-items: center;
     margin-bottom: 40px;
     gap: 40px;
-
+    @media screen and (max-width: 789px) {
+      flex-direction: column;
+      justify-content: start;
+    }
     &__input {
       padding: 12px 10px;
       width: 100%;
@@ -299,18 +482,28 @@ const thirdList = ref([
       display: flex;
       flex-direction: column;
       gap: 5px;
+      @media screen and (max-width: 789px) {
+      align-items: start;
+    }
     }
   }
 }
 
 .application-table {
   max-width: 1115px;
-  overflow: auto;
+  overflow-x: auto;
+  max-width: 100%;
 
   table {
     margin-top: 40px;
     border-collapse: collapse;
-
+    width: 100%;
+    @media screen and (max-width: 768px) {
+        margin-top: 10px;
+      }
+    tr {
+      width: 100%;
+    }
     thead {
       border: 1px solid #cdcdcd;
       border-bottom: 2px solid #cdcdcd;
@@ -388,5 +581,44 @@ const thirdList = ref([
       color: #1baa75;
     }
   }
+}
+.tooltip {
+  position: relative;
+}
+.tooltip span {
+  visibility: hidden;
+  width: 320px;
+  background: #ffffff;
+  color: #000;
+  text-align: start;
+  border-radius: 6px;
+  padding: 16px 16px;
+  box-sizing: border-box;
+  position: absolute;
+  z-index: 9;
+  top: -1em;
+  left: 100%;
+  margin-left: 1em;
+  opacity: 0;
+  transition: opacity 1s;
+  display: flex;
+  justify-content: space-between;
+}
+.tooltip span::after {
+  content: "";
+  position: absolute;
+  top: 1.5em;
+  right: 100%;
+  margin-top: -5px;
+  border-width: 5px;
+  border-style: solid;
+  border-color: transparent white transparent transparent;
+}
+.tooltip input {
+  display: none;
+}
+.tooltip input:checked + span {
+  visibility: visible;
+  opacity: 1;
 }
 </style>
