f8451d1a7a434509991d28eda475b4b3d79cd02d
Seitek2002 <seitek_2002@bk.ru>
2023-05-31 10:23:40 +0600

    adaptive

--- src/views/notarius/NotariusList.vue
+++ src/views/notarius/NotariusList.vue
@@ -226,7 +226,7 @@ onMounted(() => {
     box-shadow: 0 5px 12px -5px rgba(174, 174, 174, 0.06),
       0 5px 30px -5px rgba(160, 160, 160, 0.1);
     background: #1baa75;
-    border-radius: 2px;
+    border-radius: 100%;
     text-align: center;
   }
 
